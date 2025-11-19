8ca2f95d3360044af504d0f32e67c8ce6f20eb5a
Felipe William <felipe.william.silva@outlook.com>
2025-11-19 19:49:12 -0400

    :sparkles: feat: criação do arquivo das promoções com implementação dos modelos e associações #5

---
+++ src/models/Promotion.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize, DataTypes) => {
+    
+  const Promotion = sequelize.define('Promotion', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    produto_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+    percentual_desconto: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    data_inicio: {
+      type: DataTypes.DATE, 
+      allowNull: false
+    },
+    data_fim: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    esta_ativo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+      allowNull: false
+    },
+    criado_em: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    }
+  }, {
+    tableName: 'promocoes',
+    timestamps: false
+  });
+
+  return Promotion;
+};
