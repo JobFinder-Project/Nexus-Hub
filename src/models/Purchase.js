b3ca37197b173b7f8b8030ab2ca91bc44010e57c
Felipe William <felipe.william.silva@outlook.com>
2025-11-19 19:53:11 -0400

    :sparkles: feat: add configuração do model da compra #5

---
+++ src/models/Purchase.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, DataTypes) => {
+
+  const Purchase = sequelize.define('Purchase', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    cliente_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    data_compra: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false
+    },
+    preco_total: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    status_pagamento: {
+      type: DataTypes.STRING, // 'pendente', 'aprovado', 'recusado'
+      defaultValue: 'pendente',
+      allowNull: false
+    }
+  }, {
+    tableName: 'compras',
+    timestamps: false 
+  });
+
+  return Purchase;
+};
